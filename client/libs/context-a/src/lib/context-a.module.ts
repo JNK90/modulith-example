392c2184ef13caf37b37aedab7709e46a145a223
Jan-Niklaas Koch <jan-niklaas.koch@ovsoftware.com>
2021-12-08 10:50:44 +0100

    add example for routing in lib

--- client/libs/context-a/src/lib/context-a.module.ts
+++ client/libs/context-a/src/lib/context-a.module.ts
@@ -2,18 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContextAComponent } from './context-a/context-a.component';
 import { Route, RouterModule } from '@angular/router';
+import { ExampleComponent } from './context-a/example/example.component';
 
 const routes: Route[] = [
   {
     path: '',
-    component: ContextAComponent
-  }
+    component: ContextAComponent,
+    children: [
+      {
+        path: 'example',
+        component: ExampleComponent
+      }
+
+    ]
+  },
 ];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [
-    ContextAComponent
+    ContextAComponent,
+    ExampleComponent
   ],
   exports: [
     ContextAComponent
